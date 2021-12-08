930482a7bfe259ee8d89f54d2042b9c1a653bd63
Mohammad Haroun <harounmohammad@yahoo.com>
2021-12-08 19:09:59 +0200

    finish

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useReducer } from "react";
 
 import "./app.scss";
 
@@ -8,88 +8,122 @@ import Header from "./components/header/index";
 import Footer from "./components/footer/index";
 import Form from "./components/form/index";
 import Results from "./components/results/index";
-import axios from 'axios'
+import axios from "axios";
+import History from "./components/History/history";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+let initialState = {
+  data: {},
+  requestParams: {},
+  history: [],
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "requestParamsAction":
+      return { ...state, requestParams: action.payload };
+
+    case "dataAction":
+      return { ...state, data: action.payload };
+
+    case "historyAction":
+      return { ...state, history: [action.payload, ...state.history] };
+
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  // const [history,setHistory]=useState([])
 
-  let [data,setData]=useState(null)
-  let [requestParams,setRequestParams ]=useState({})
+  // let [data,setData]=useState(null)
+  // let [requestParams,setRequestParams ]=useState({})
 
+  useEffect(
+    async () => {
+      let newEvent = {
+        Body: [],
+        Headers: {},
+      };
 
-  useEffect( async()=>{
-    let newEvent={
-      Body:[],
-      Headers: {}
-    };
+      if (state.requestParams.method === "get") {
+        await axios.get(`${state.requestParams.url}`).then((collected) => {
+          console.log("collected", collected);
+          newEvent = { Body: collected.data, Headers: collected.headers };
+        });
+      }
 
+      if (state.requestParams.method === "post") {
+        let body = state.requestParams.body;
+        await axios
+          .post(`${state.requestParams.url}`, body)
+          .then((collected) => {
+            console.log("collected", collected);
+            newEvent = { Body: collected.data, Headers: collected.headers };
+          });
+      }
 
-    if (requestParams.method==='get') {
-      await axios.get(`${requestParams.url}`).then(collected=>{
-        console.log('collected',collected);
-         newEvent={Body : collected.data,
-            Headers : collected.headers} 
-      }) 
-    
-    }
+      let count = newEvent.Body.length;
 
-    if (requestParams.method==='post') {
-      let body=requestParams.body
-      await axios.post(`${requestParams.url}`,body).then(collected=>{
-       
-        console.log('collected',collected);
-         newEvent={Body : collected.data,
-            Headers : collected.headers} 
-      }) 
-    
-    }
+      // console.log('newEvent',newEvent);
+      const data = {
+        count: count,
+        results: newEvent,
+      };
+      // data.results=newEvent
+      // console.log('dataaaaaaa',data);
 
+      dispatch({ type: "dataAction", payload: data });
 
+      // setData(data);
+      const testhistory = {
+        data: data,
+        requestParams: state.requestParams,
+      };
 
-   
+      dispatch({ type: "historyAction", payload: testhistory });
 
-    let count = newEvent.Body.length
-    
+      // setHistory()
+    },
+    [state.requestParams],
+    [state.history]
+  );
 
-    // console.log('newEvent',newEvent);
-    const data = {
-
-      count: count,
-      results: newEvent
-    };
-
-    // data.results=newEvent
-
-    console.log('dataaaaaaa',data);
-    setData(data);
-    
-  },[requestParams])
-
-  
-  
   const callApi = async (requestParams) => {
-
-    setRequestParams(requestParams)
-    // mock output
-    
+    dispatch({ type: "requestParamsAction", payload: requestParams });
   };
 
-  // const options = {
-  //   method: 'post',
-  //   url: '/login',
-  //   data: {
-  //     firstName: 'Finn',
-  //     lastName: 'Williams'
-  //   },
-  // axios(options);
+  //   console.log('state',state);
+  // console.log('state.requestParams.url',state.requestParams.url);
 
   return (
     <React.Fragment>
-      <Header />
-      <div data-testid='method'>Request Method: {requestParams.method}</div>
-      <div>URL: {requestParams.url}</div>
-      <Form handleApiCall={callApi} />
-      <Results data={data} />
-      <Footer />
+      {console.log("+++++++++++++", state.history)}
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route exact path="/Home">
+            
+            <div data-testid="method">
+              Request Method: {state.requestParams.method}
+            </div>
+            <div>URL: {state.requestParams.url}</div>
+            <Form handleApiCall={callApi} />
+            <Results data={state.data} />
+            <History dataHistory={state.history} />
+          </Route>
+          <Route exact path="/History">
+           
+            <History dataHistory={state.history} />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </Router>
     </React.Fragment>
   );
 }
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,7 +6,26 @@ export default function header() {
   return (
     <header>
       <h1>RESTy</h1>
+
+      <ul>
+      
+        <li>
+          <a  href="/Home">Home</a>
+
+        </li>
+        <li>
+        <a  href="/History">History</a>
+
+        </li>
+        <li>
+        <a  href="/Help">Help</a>
+        </li>
+        
+      
+    </ul>
+
     </header>
+    
   );
   
 }
